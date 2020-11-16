1b2a68e662cd3082fc5c3eb9fe7a474efe4803f3
vangasse <vangasse@ic.ufal.br>
2020-11-16 18:50:14 -0300

    added special damping to treat singularities

--- manipulator_model/src/compliant.py
+++ manipulator_model/src/compliant.py
@@ -14,11 +14,11 @@ def updateWorld(msg):
     soft_snake.setJointStates(joint_states)
 
 def cb_velocity(msg):
-    global q
-    q = np.array([msg.linear.x, msg.linear.y, msg.linear.z, msg.angular.x, msg.angular.y, msg.angular.z]).T
+    global x
+    x = np.array([msg.linear.x, msg.linear.y, msg.linear.z, msg.angular.x, msg.angular.y, msg.angular.z]).T
 
-soft_snake = Robot([0, 0, 0, 0, 0, 0], [1, 1, 1, 1], 0, 0, 0)
-q = np.array([0, 0, 0, 0, 0, 0])
+soft_snake = Robot([0, 0, 0, 0, 0, 0], [0.155, 0.12, 0.12, 0.03], 0, 0, 0)
+x = np.array([0, 0, 0, 0, 0, 0])
 Ts = 0.1
 
 rospy.init_node('compliant_control')
@@ -30,19 +30,20 @@ rospy.Subscriber('/cmd_vel', Twist, cb_velocity)
 pub = rospy.Publisher('/manipulator_pos_group_controller/command', Float64MultiArray, queue_size=10)
 
 i = 0
+lbd = 0.5
+
 while not rospy.is_shutdown():
     J = soft_snake.jacobianComputation()
-    w = np.dot(np.linalg.pinv(J), q)*0.0001
+    w = np.dot(np.dot(np.linalg.inv(np.dot(np.transpose(J), J) + (lbd ** 2) * np.eye(5)), np.transpose(J)), x)
+    #w = np.dot(np.linalg.pinv(J), x)
     joint_cmd = Float64MultiArray()
     joint_cmd.data = [0, 0, soft_snake.getJointStates()[0] + w[0]*Ts, soft_snake.getJointStates()[1] + w[1]*Ts,
                         soft_snake.getJointStates()[2] + w[2]*Ts, soft_snake.getJointStates()[3] + w[3]*Ts]
     pub.publish(joint_cmd)
 
-    if i > 10:
-        print(q, joint_cmd)
-        i = 0
-    else:
-        i+=1
+    
+    print(x, w)
+    
     pass
 
     rospy.sleep(Ts)
