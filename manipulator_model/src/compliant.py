8b3eb1c8a38757471500ddcbe1854970f6944074
Vangasse <vangasse@ic.ufal.br>
2020-11-17 17:44:22 -0300

    removed damping

--- manipulator_model/src/compliant.py
+++ manipulator_model/src/compliant.py
@@ -17,7 +17,7 @@ def cb_velocity(msg):
     global x
     x = np.array([msg.linear.x, msg.linear.y, msg.linear.z, msg.angular.x, msg.angular.y, msg.angular.z]).T
 
-soft_snake = Robot([0, 0, 0, 0, 0, 0], [0.155, 0.12, 0.12, 0.03], 0, 0, 0)
+soft_snake = Robot([0, 0, 0, 0, 0, 0], [0.1825, 0.12, 0.12, 0.03], 0, 0, 0)
 x = np.array([0, 0, 0, 0, 0, 0])
 Ts = 0.1
 
@@ -30,12 +30,12 @@ rospy.Subscriber('/cmd_vel', Twist, cb_velocity)
 pub = rospy.Publisher('/manipulator_pos_group_controller/command', Float64MultiArray, queue_size=10)
 
 i = 0
-lbd = 0.5
+lbd = 0.1
 
 while not rospy.is_shutdown():
     J = soft_snake.jacobianComputation()
-    w = np.dot(np.dot(np.linalg.inv(np.dot(np.transpose(J), J) + (lbd ** 2) * np.eye(5)), np.transpose(J)), x)
-    #w = np.dot(np.linalg.pinv(J), x)
+    #w = np.dot(np.dot(np.linalg.inv(np.dot(np.transpose(J), J) + (lbd ** 2) * np.eye(5)), np.transpose(J)), x)
+    w = np.dot(np.linalg.pinv(J), x)
     joint_cmd = Float64MultiArray()
     joint_cmd.data = [0, 0, soft_snake.getJointStates()[0] + w[0]*Ts, soft_snake.getJointStates()[1] + w[1]*Ts,
                         soft_snake.getJointStates()[2] + w[2]*Ts, soft_snake.getJointStates()[3] + w[3]*Ts]
